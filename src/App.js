0b828cffb6714952864070f8c704ea2fd1f39dc9
ganesh2425 <saahoganesh023@gmail.com>
2021-07-21 07:51:10 +0530

    co

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from "react";
-import Login from "./RBasics/Forms/Login";
-function App (){
-  return(<>
-  <h>Hello Welcome to React</h>
-  <Login/>
-  </>);
+class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={counter: 0}
+    }
+incrHandler=()=>{
+    this.setState({counter: this.state.counter +1})
+}
+decrHandler=()=>{
+    this.setState({counter: this.state.counter -1})
+}
+    render(){
+        return(<>
+        <nav className="navbar navbar-dark bg-dark navbar-expand-sm">
+            <a href="/" className="navbar-brand">Counter</a>
+        </nav>
+        <h2>Hello Counter {this.state.counter}</h2>
+        <button onClick={this.incrHandler} className="btn btn-success mr-4">Incr</button>
+        <button onClick={this.decrHandler} className="btn btn-danger">Decr</button>
+        </>);
+    }
 }
 export default App;
